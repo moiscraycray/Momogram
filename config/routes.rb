99778404c99590c0d02a5ea5afcf0f6807783737
Olivia Mo <oliviatrademark@gmail.com>
2017-10-13 18:11:04 +1100

    added bootstrap, installed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/index'
 
-  get 'pages/about'
+  devise_for :users
+  root 'pages#index'
+
+  get '/about', to: 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
